b9ce269b8e6193e461cd1477f42347dd81be1377
AdamMenard <adammenard888@gmail.com>
2018-02-14 12:59:38 -0800

    pair programming, w/ Sherif: added multiple views for MPA

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users.create[
+  {
+  name:"John Harvy",
+  location:"San Francisco",
+  dating_preference:"F",
+  age:"28",
+  gender:"M"
+  },
+  {
+  name:"Jane Goodall",
+  location:"San Francisco",
+  dating_preference:"M",
+  age:"27",
+  gender:"F"
+  },
+  name:"Sam Bacon",
+  location:"Oakland",
+  dating_preference:"F",
+  age:"24",
+  gender:"F"
+  },
+  {
+  name:"Amy Fowler",
+  location:"Oakland",
+  dating_preference:"F",
+  age:"26",
+  gender:"F"
+  },
+
+]
+
+
+Restaurant.create(
+  name:"Trestle",
+  location:"San Francisco",
+  img_url:"https://static.urbandaddy.com/uploads/assets/image/slideshows/standard/da58dc9fa6e0f3a06ef35ab731c1c01e.jpg",
+  description:"New American restaurant offering 3-course fixed-price meals in a quaint space with modern flair."
+)
